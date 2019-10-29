e26102e75e62be3802d1f8f9c5d74ae2ec9dc26f
yanyisong <1121225022@qq.com>
2019-10-29 22:30:26 +0800

    1

---
+++ client.py
@@ -0,0 +1,15 @@
+
+msMg = MessageManager()
+
+ds = DataSet()
+
+model = Module()
+
+
+train(model,ds1,ds2,msMg) # NOTE 是一个解
+
+
+def train(model,ds1,ds2,msMg):
+
+
+
---
+++ msmg.py
@@ -0,0 +1,43 @@
+
+"""消息管理器"""
+
+
+class MqConn(object):
+    def band(self,projectName):
+        pass
+
+    def push(self,a,topic):
+        pass
+    
+    def pull(self,topic):
+        pass
+
+    def clear(self,projectName):
+        pass
+
+
+class Serializer(object):
+    def serialize(self,a):
+        pass
+    def serialize_inv(self,b):
+        pass
+
+
+
+class MessageManager(object):
+    
+    def __init__(self):
+        self.mqConn = None
+        self.serializer = None
+
+    def push(self,data,topic):
+        pass
+    
+    def pull(self,topic):
+        pass
+
+
+
+
+
+
